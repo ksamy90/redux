81cc54e441435c3db9bb47d72dd79a4a193ac1ef
ksamy90 <ksamy90@aol.com>
2020-07-21 12:02:53 +0300

    destructuring action creators

--- src/redux/finance.js
+++ src/redux/finance.js
@@ -2,16 +2,41 @@ import { createStore, combineReducers } from "redux";
 import uuid from "uuid";
 
 // ADD_EXPENSE
-var addExpense = (payload = {}) => {
+// var addExpense = (payload = {}) => {
+//   return {
+//     type: "ADD_EXPENSE",
+//     expense: {
+//       id: uuid(),
+//       description: payload.description ? payload.description : "",
+//       note: payload.note ? payload.note : "",
+//       amount: payload.amount ? payload.amount : 0,
+//       createdAt: payload.createdAt ? payload.createdAt : 0,
+//     },
+//   };
+// };
+var addExpense = ({
+  description = "",
+  note = "",
+  amount = 0,
+  createdAt = 0,
+} = {}) => {
   return {
     type: "ADD_EXPENSE",
     expense: {
       id: uuid(),
-      description: payload.description ? payload.description : "",
-      note: payload.note ? payload.note : "",
-      amount: payload.amount ? payload.amount : 0,
-      createdAt: payload.createdAt ? payload.createdAt : 0,
+      description,
+      note,
+      amount,
+      createdAt,
     },
+  };
+};
+
+// REMOVE_EXPENSE
+const removeExpense = (payload = {}) => {
+  return {
+    type: "REMOVE_EXPENSE",
+    id: payload.id,
   };
 };
 
@@ -20,11 +45,15 @@ var expensesReducerDefaultState = [];
 var expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
     case "ADD_EXPENSE":
-      return state.concat(action.expense);
+      return [...state, action.expense];
+    case "REMOVE_EXPENSE":
+      return state.filter((expense) => expense.id !== action.id);
     default:
       return state;
   }
 };
+
+// [].concat('joseph') --- ['joseph'] // [].concat({ description: "Electric Bills", amount: 200 })
 
 // filters reducer
 var filtersReducerDefaultState = {
@@ -51,4 +80,11 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(addExpense({ description: "Electric Bills", amount: 200 }));
+var expenseOne = store.dispatch(
+  addExpense({ description: "Electric Bills", amount: 200 })
+);
+var expenseTwo = store.dispatch(
+  addExpense({ description: "Reducer in hooks", amount: 500 })
+);
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
